b87648b824b0b28c2d15934c0462aa697076e285
Vijayasri I. <thisisvij98@gmail.com>
2024-06-06 19:52:34 +0530

    fully working agent commit

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import os
 from dotenv import load_dotenv
-from langchain_community.document_loaders import TextLoader
+from langchain_mistralai.chat_models import ChatMistralAI
+from langchain_mistralai.embeddings import MistralAIEmbeddings
+from langchain_mistralai.chat_models import ChatMistralAI
 from langchain_community.vectorstores import Qdrant
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_mistralai.chat_models import ChatMistralAI
-from langchain_mistralai.embeddings import MistralAIEmbeddings
-from langchain_community.document_loaders import TextLoader
-from langchain_mistralai.chat_models import ChatMistralAI
-from langchain_mistralai.embeddings import MistralAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
-from langchain_community.document_loaders import WebBaseLoader
 from qdrant_client import QdrantClient
 import streamlit as st
+
+from eove_agent.eove_prompt_templates import (
+    retrieval_grader_prompt,
+    rag_chain_prompt,
+    hallucination_grader_prompt,
+    answer_grader_prompt,
+    question_router_prompt,
+)
+
+from typing_extensions import TypedDict
+from typing import List
+from langchain_core.documents import Document
+
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+
+from langgraph.graph import END, StateGraph
+
+
+SOURCE = None
 
 load_dotenv()
 mistral_api_key = os.environ.get("MISTRAL_API_KEY")
@@ -22,7 +33,7 @@ qdrant_url = os.environ.get("QDRANT_URL")
 qdrant_api_key = os.environ.get("QDRANT_API_KEY")
 embeddings = MistralAIEmbeddings(model="mistral-embed", mistral_api_key=mistral_api_key)
 
-
+### DEFINING DATABASE AND LLM
 client = QdrantClient(
     qdrant_url,
     api_key=qdrant_api_key,  # For Qdrant Cloud, None for local instance
@@ -34,20 +45,265 @@ doc_store = Qdrant(
     embeddings=embeddings,
 )
 
-
 retriever = doc_store.as_retriever(search_type="mmr", search_kwargs={"k": 3})
-client = QdrantClient(url="http://localhost:6333")
+
+llm = ChatMistralAI(mistral_api_key=mistral_api_key)
+
+### DEFINING COMPONENTS
+retrieval_grader = retrieval_grader_prompt | llm | JsonOutputParser()
+rag_chain = rag_chain_prompt | llm | StrOutputParser()
+hallucination_grader = hallucination_grader_prompt | llm | JsonOutputParser()
+answer_grader = answer_grader_prompt | llm | JsonOutputParser()
+question_router = question_router_prompt | llm | JsonOutputParser()
+web_search_tool = TavilySearchResults(k=3)
 
 
-model = ChatMistralAI(mistral_api_key=mistral_api_key)
-prompt = ChatPromptTemplate.from_template(
-    """Answer the following question based only on the provided context:
+#### DEFINE THE GRAPH STATE AND ITS NODES
+### State
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
 
-<context>
-{context}
-</context>
+    Attributes:
+        question: question
+        generation: LLM generation
+        web_search: whether to add search
+        documents: list of documents
+    """
 
-Question: {input}"""
+    question: str
+    generation: str
+    web_search: str
+    documents: List[str]
+
+
+### Nodes
+def retrieve(state):
+    """
+    Retrieve documents from vectorstore
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, documents, that contains retrieved documents
+    """
+    print("---RETRIEVE---")
+    question = state["question"]
+
+    # Retrieval
+    documents = retriever.invoke(question)
+    return {"documents": documents, "question": question}
+
+
+def generate(state):
+    """
+    Generate answer using RAG on retrieved documents
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, generation, that contains LLM generation
+    """
+    print("---GENERATE---")
+    question = state["question"]
+    documents = state["documents"]
+
+    # RAG generation
+    generation = rag_chain.invoke({"context": documents, "question": question})
+    return {"documents": documents, "question": question, "generation": generation}
+
+
+def grade_documents(state):
+    """
+    Determines whether the retrieved documents are relevant to the question
+    If any document is not relevant, we will set a flag to run web search
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Filtered out irrelevant documents and updated web_search state
+    """
+
+    print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
+    question = state["question"]
+    documents = state["documents"]
+
+    # Score each doc
+    filtered_docs = []
+    web_search = "No"
+    for d in documents:
+        score = retrieval_grader.invoke(
+            {"question": question, "document": d.page_content}
+        )
+        grade = score["score"]
+        # Document relevant
+        if grade.lower() == "yes":
+            print("---GRADE: DOCUMENT RELEVANT---")
+            filtered_docs.append(d)
+        # Document not relevant
+        else:
+            print("---GRADE: DOCUMENT NOT RELEVANT---")
+            # We do not include the document in filtered_docs
+            # We set a flag to indicate that we want to run web search
+            web_search = "Yes"
+            continue
+    return {"documents": filtered_docs, "question": question, "web_search": web_search}
+
+
+def web_search(state):
+    """
+    Web search based based on the question
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Appended web results to documents
+    """
+
+    print("---WEB SEARCH---")
+    question = state["question"]
+    documents = state["documents"]
+
+    # Web search
+    docs = web_search_tool.invoke({"query": question})
+    return {"documents": docs, "question": question}
+
+
+### Conditional edge
+def route_question(state):
+    """
+    Route question to web search or RAG.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Next node to call
+    """
+
+    print("---ROUTE QUESTION---")
+    question = state["question"]
+    print(question)
+    source = question_router.invoke({"question": question})
+    print(source)
+    print(source["datasource"])
+    if source["datasource"] == "web_search":
+        print("---ROUTE QUESTION TO WEB SEARCH---")
+        return "websearch"
+    elif source["datasource"] == "vectorstore":
+        print("---ROUTE QUESTION TO RAG---")
+        return "vectorstore"
+
+
+def decide_to_generate(state):
+    """
+    Determines whether to generate an answer, or add web search
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Binary decision for next node to call
+    """
+
+    print("---ASSESS GRADED DOCUMENTS---")
+    question = state["question"]
+    web_search = state["web_search"]
+    filtered_documents = state["documents"]
+
+    if web_search == "Yes":
+        # All documents have been filtered check_relevance
+        # We will re-generate a new query
+        print(
+            "---DECISION: ALL DOCUMENTS ARE NOT RELEVANT TO QUESTION, INCLUDE WEB SEARCH---"
+        )
+        return "websearch"
+    else:
+        # We have relevant documents, so generate answer
+        print("---DECISION: GENERATE---")
+        return "generate"
+
+
+### Conditional edge
+def grade_generation_v_documents_and_question(state):
+    """
+    Determines whether the generation is grounded in the document and answers question.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Decision for next node to call
+    """
+
+    print("---CHECK HALLUCINATIONS---")
+    question = state["question"]
+    documents = state["documents"]
+    generation = state["generation"]
+
+    score = hallucination_grader.invoke(
+        {"documents": documents, "generation": generation}
+    )
+    grade = score["score"]
+
+    # Check hallucination
+    if grade == "yes":
+        print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
+        # Check question-answering
+        print("---GRADE GENERATION vs QUESTION---")
+        score = answer_grader.invoke({"question": question, "generation": generation})
+        grade = score["score"]
+        if grade == "yes":
+            print("---DECISION: GENERATION ADDRESSES QUESTION---")
+            return "useful"
+        else:
+            print("---DECISION: GENERATION DOES NOT ADDRESS QUESTION---")
+            return "not useful"
+    else:
+        pprint("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---")
+        return "not supported"
+
+
+workflow = StateGraph(GraphState)
+
+# Define the nodes
+workflow.add_node("websearch", web_search)  # web search
+workflow.add_node("retrieve", retrieve)  # retrieve
+workflow.add_node("grade_documents", grade_documents)  # grade documents
+workflow.add_node("generate", generate)  # generatae
+
+
+# Build graph
+workflow.set_conditional_entry_point(
+    route_question,
+    {
+        "vectorstore": "retrieve",
+        "websearch": "websearch",
+    },
+)
+
+workflow.add_edge("retrieve", "grade_documents")
+workflow.add_conditional_edges(
+    "grade_documents",
+    decide_to_generate,
+    {
+        "websearch": "websearch",
+        "generate": "generate",
+    },
+)
+workflow.add_edge("websearch", "generate")
+workflow.add_conditional_edges(
+    "generate",
+    grade_generation_v_documents_and_question,
+    {
+        "not supported": "generate",
+        "useful": END,
+        "not useful": "websearch",
+    },
 )
 
 
@@ -66,14 +322,22 @@ if query := st.chat_input("What is up?"):
         st.markdown(query)
 
     with st.chat_message("assistant"):
-        # Create a retrieval chain to answer questions
-        document_chain = create_stuff_documents_chain(model, prompt)
-        retrieval_chain = create_retrieval_chain(retriever, document_chain)
-        response = retrieval_chain.invoke({"input": query})
-        st.write(response["answer"])
-        for doc in response["context"]:
-            st.text(doc.metadata["title"])
-            st.markdown(doc.metadata["source"])
+        # Compile
+        inputs = {"question": query}
+        app = workflow.compile()
+        source_docs = []
+        for output in app.stream(inputs):
+            for key, value in output.items():
+                for document in value["documents"]:
+                    if isinstance(document, Document):
+                        if "source" in document.metadata:
+                            source_docs.append(document.metadata["source"])
+                    elif isinstance(document, dict):
+                        source_docs.append(document["url"])
+        st.text(value["generation"])
+        for doc in source_docs:
+            st.markdown(doc)
+
     st.session_state.messages.append(
-        {"role": "assistant", "content": response["answer"]}
+        {"role": "assistant", "content": value["generation"]}
     )
