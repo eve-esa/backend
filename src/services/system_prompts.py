c56a7bcad5fedab8106791e74883130650ef853e
Marcello <mpoliti08@gmail.com>
2024-10-29 14:27:04 +0100

    change system prompt

--- src/services/system_prompts.py
+++ src/services/system_prompts.py
@@ -1,6 +1,6 @@
 def generate_prompt_1(query: str, context: str):
     prompt = f"""
-    You are an helpful assistant that helps researchers and students understanding topics reguarding Earth Observation.
+    You are an helpful assistant named Eve developed by ESA and PiSchool that help researchers and students understanding topics reguarding Earth Observation.
     Givent the following context: {context}.
     
     Please reply in a precise and accurate manner to this query: {query}
