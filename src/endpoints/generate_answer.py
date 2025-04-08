469de6737f73a8b45129d964a7adc96e17b77a9a
Marcello <mpoliti08@gmail.com>
2025-04-08 09:44:24 +0200

    feat: use rag function

--- src/endpoints/generate_answer.py
+++ src/endpoints/generate_answer.py
@@ -2,20 +2,72 @@ from fastapi import APIRouter, HTTPException
 from typing import Dict, Any
 from pydantic import BaseModel, Field
 from src.services.vector_store_manager import VectorStoreManager
-from src.config import QDRANT_URL, QDRANT_API_KEY
+from src.config import QDRANT_URL, QDRANT_API_KEY, OPENAI_API_KEY
+from openai import OpenAI, Client
 
 router = APIRouter()
+openai_client = Client(api_key=OPENAI_API_KEY)
 
 
 class GenerationRequest(BaseModel):
     query: str = "What is ESA?"
     collection_name: str = "esa-nasa-workshop"
-    llm: str = "openai"
+    llm: str = "eve-instruct-v0.1"  # or openai
     embeddings_model: str = "text-embedding-3-small"
     k: int = 3
     score_threshold: float = Field(0.7, ge=0.0, le=1.0)  # Ensure it's between 0 and 1
     get_unique_docs: bool = True
     max_new_tokens: int = Field(1500, ge=100, le=8192)
+
+
+def use_rag(query: str) -> bool:
+    prompt = f"""
+    You should decide if using RAG or not to answer to a given query. Use RAG when the query
+    is abuot earth science, space science, climate, space agencies and similar topis.
+    You should answer only with 'yes' or 'no'.
+    
+    Query: {query}
+    """
+
+    response = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}],
+        max_tokens=10,
+        temperature=0,
+    )
+
+    if response.choices:
+        answer = response.choices[0].message.content.strip()
+        if answer.lower() == "yes":
+            print("I am using rag...")
+            return True
+        elif answer.lower() == "no":
+            print("I am not using rag...")
+            return False
+        else:
+            raise ValueError("Unexpected response from OpenAI API")
+
+
+def get_rag_context(
+    vector_store: VectorStoreManager, request: GenerationRequest
+) -> str:
+    vector_store = VectorStoreManager(
+        QDRANT_URL, QDRANT_API_KEY, embeddings_model=request.embeddings_model
+    )
+    results = vector_store.retrieve_documents_from_query(
+        query=request.query,
+        collection_name=request.collection_name,
+        embeddings_model=request.embeddings_model,
+        score_threshold=request.score_threshold,
+        get_unique_docs=request.get_unique_docs,
+        k=request.k,
+    )
+    if not results:
+        print(f"No documents found for query : {request.query}")
+
+    retrieved_documents = [result.payload.get("page_content", "") for result in results]
+    context = "\n".join(retrieved_documents)
+    return context, results
 
 
 @router.post("/generate_answer", response_model=Dict[str, Any])
@@ -24,29 +76,16 @@ def create_collection(request: GenerationRequest) -> Dict[str, Any]:
         vector_store = VectorStoreManager(
             QDRANT_URL, QDRANT_API_KEY, embeddings_model=request.embeddings_model
         )
-        results = vector_store.retrieve_documents_from_query(
-            query=request.query,
-            collection_name=request.collection_name,
-            embeddings_model=request.embeddings_model,
-            score_threshold=request.score_threshold,
-            get_unique_docs=request.get_unique_docs,
-            k=request.k,
+        is_rag: bool = use_rag(request.query)
+        context, results = (
+            get_rag_context(vector_store, request) if is_rag else ("", [])
         )
-        if not results:
-            print(f"No documents found for query : {request.query}")
-
-        retrieved_documents = [
-            result.payload.get("page_content", "") for result in results
-        ]
-        context = "\n".join(retrieved_documents)
-
         answer = vector_store.generate_answer(
             query=request.query,
             context=context,
             llm=request.llm,
             max_new_tokens=request.max_new_tokens,
         )
-
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
