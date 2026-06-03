a8a2d0f299a322c572dbb3fe4a1e34e55ab179a0
Hiroshi Araki <hiroshiaraki999@gmail.com>
2026-06-03 15:43:44 +0100

    fix: use collection name for wikipedia-512

--- src/constants.py
+++ src/constants.py
@@ -64,7 +64,7 @@ STAGING_PUBLIC_COLLECTIONS = [
         "description": "This collection brings together Wikipedia articles related to Earth Observation (EO). The content is intended to provide accessible, introductory information about EO concepts, technologies, and organizations active in the field. Please note that these articles are not peer-reviewed scientific publications. Instead, they are written for a general audience and aim to give broad overviews rather than in-depth, expert analyses. Users should treat this collection as a starting point for understanding EO, and complement it with specialized, peer-reviewed sources when deeper or technical knowledge is required. This collection contains about 2000 documents.",
     },
     {
-        "name": "Wikipedia EO",
+        "name": "wikipedia-512",
         "description": "ESA data with Wikipedia articles for testing",
     },
     {"name": "EVE open access", "description": "ESA data with Qwen-512 for testing"},
